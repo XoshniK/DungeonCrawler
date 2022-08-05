13168d8e4e405724199e1ffc4cd2b924c845a991
David <tegashvili.david@gmail.com>
2022-08-05 20:25:09 +0200

    Random movement

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,9 +1,11 @@
 mod render;
 mod player;
 mod enemy;
+mod moving_randomly;
 
 pub mod prelude {
     pub use super::render::*;
     pub use super::player::*;
     pub use super::enemy::*;
+    pub use super::moving_randomly::*;
 }
--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -4,12 +4,15 @@ mod player_input;
 mod map_render;
 mod entity_render;
 mod collisions;
+mod random_movement;
 
 pub fn build_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(player_input::player_input_system())
+        .add_system(collisions::collisions_system())
+        .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
-        .add_system(collisions::collisions_system())
+        .add_system(random_movement::random_move_system())
         .build()
 }
